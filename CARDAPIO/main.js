83c78a25a9df0c74f32817c2675201c93d896263
Wendel by Windowns <wh68639@gmail.com>
2023-02-17 11:05:39 -0300

    js arrumado

--- CARDAPIO/main.js
+++ CARDAPIO/main.js
@@ -3,48 +3,115 @@ const bebida = document.querySelector('#bebida');
 const sobremesa = document.querySelector('#sobremesa');
 const todas = document.querySelector("#todos");
 
-const aloha = document.querySelector("#aloha")
-const mar = document.querySelector("#mar")
+const comercial = document.querySelector("#comercial");
 const ceviche = document.querySelector("#ceviche")
+const penne = document.querySelector("#penne")
 const pirapuke = document.querySelector("#pirapuke")
-const oahu = document.querySelector("#oahu")
-const carb = document.querySelector("#carb")
-const coxinha = document.querySelector("#coxinha")
-const brigadeiro = document.querySelector("#brigadeiro")
-const penne = document.querySelector("#pennes")
+const aloha = document.querySelector("#aloha")
+const salada = document.querySelector("#salada")
+
+
+const agua = document.querySelector("#agua")
+const vinho = document.querySelector("#vinho")
+const refrigerante = document.querySelector("#refrigerante")
+const sucos = document.querySelector("#sucos")
+
+
+const brownie = document.querySelector("#brownie")
+const gelatina = document.querySelector("#gelatina")
+const frutas = document.querySelector("#frutas")
+const melancia = document.querySelector("#melancia")
 
 
 
 
 comida.addEventListener('click', mostrarComida)
 function mostrarComida() {
-    oahu.style.display = 'none';
-    carb.style.display = 'none';
-    brigadeiro.style.display = 'none';
-    coxinha.style.display = 'none';
-    penne.style.display = 'none';
+    agua.style.display = 'none';
+    vinho.style.display = 'none';
+    refrigerante.style.display = 'none';
+    sucos.style.display = 'none';
+
+    brownie.style.display = 'none';
+    gelatina.style.display = 'none';
+    frutas.style.display = 'none';
+    melancia.style.display = 'none';
+
+    aloha.style.display = 'flex'
+    ceviche.style.display = 'flex'
+    pirapuke.style.display = 'flex'
+    comercial.style.display = 'flex';
+    penne.style.display = 'flex';
+    salada.style.display = 'flex';
+
+
 }
 
 todos.addEventListener('click', mostrarTodos)
 
 function mostrarTodos() {
-    oahu.style.display = 'flex';
-    carb.style.display = 'flex';
-    brigadeiro.style.display = 'flex';
-    coxinha.style.display = 'flex';
-    penne.style.display = 'flex';
-    mar.style.display = 'flex'
     aloha.style.display = 'flex'
     ceviche.style.display = 'flex'
     pirapuke.style.display = 'flex'
+    comercial.style.display = 'flex';
+    penne.style.display = 'flex';
+    salada.style.display = 'flex';
 
-
+    agua.style.display = 'flex';
+    vinho.style.display = 'flex';
+    refrigerante.style.display = 'flex';
+    sucos.style.display = 'flex';
+    
+    brownie.style.display = 'flex';
+    gelatina.style.display = 'flex';
+    frutas.style.display = 'flex';
+    melancia.style.display = 'flex';
 
 }
 
+bebida.addEventListener('click', mostrarBebida)
+function mostrarBebida() {
+    aloha.style.display = 'none';
+    ceviche.style.display = 'none';
+    pirapuke.style.display = 'none';
+    comercial.style.display = 'none';
+    penne.style.display = 'none';
+    salada.style.display = 'none';
 
 
-console.log(comida);
+    brownie.style.display = 'none';
+    gelatina.style.display = 'none';
+    frutas.style.display = 'none';
+    melancia.style.display = 'none';
+
+    agua.style.display = 'flex';
+    vinho.style.display = 'flex';
+    refrigerante.style.display = 'flex';
+    sucos.style.display = 'flex';
+}
+
+sobremesa.addEventListener('click', mostrarSobremesa)
+function mostrarSobremesa() {
+    aloha.style.display = 'none';
+    ceviche.style.display = 'none';
+    pirapuke.style.display = 'none';
+    comercial.style.display = 'none';
+    penne.style.display = 'none';
+    salada.style.display = 'none';
+
+
+    agua.style.display = 'none';
+    vinho.style.display = 'none';
+    refrigerante.style.display = 'none';
+    sucos.style.display = 'none';
+
+    brownie.style.display = 'flex';
+    gelatina.style.display = 'flex';
+    frutas.style.display = 'flex';
+    melancia.style.display = 'flex';
+}
+
+
 
 
 
